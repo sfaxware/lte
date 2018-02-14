f0647594d4f62faebf4b780cad21f4286f5ab055
Mazen NEIFER <mazen.neifer@supaero.org>
2018-02-14 10:14:35 +0100

    Added a new port to Block21.

--- Block21.pas
+++ Block21.pas
@@ -13,6 +13,7 @@ type
   TBlock21 = class(TBlock)
     OutputPort22: TOutputPort;
     InputPort23: TInputPort; 
+    InputPort39: TInputPort; 
   public
     procedure Execute; override;
   end;
@@ -25,11 +26,13 @@ uses
 procedure TBlock21.Execute;
 var
   S: Integer;
+  N: Integer;
 begin;
   {Write here your code}
   {You may need to remove the following line}
   InputPort23.Pop(S);
-  OutputPort22.Push(S);
+  InputPort39.Pop(N);
+  OutputPort22.Push(S + N);
 end;
 
 initialization
